da29fd2df8ff45dca3e59af77d9dd2aac83af284
Andrey Bozhko <andybozhko@gmail.com>
2025-06-19 23:26:08 +0000

    update

--- scripts/update_lambda.py
+++ scripts/update_lambda.py
@@ -3,21 +3,23 @@ and update the Lambda function."""
 import os
 import shutil
 import subprocess
+import sysconfig
 from pathlib import Path
 
 import boto3
+
+py = sysconfig.get_python_version()
 
 OBJECT_NAME = "expense_bot/lambda.zip"
 
 PROJECT_DIR = Path(__file__).parent.parent.absolute()
 TMP_DIR = PROJECT_DIR / "tmp"
+DST_DIR = TMP_DIR / ".venv" / "lib" / f"python{py}" / "site-packages"
 HANDLER = PROJECT_DIR / "lambda.py"
 ARCHIVE = PROJECT_DIR / "lambda.zip"
 
 
 if __name__ == "__main__":
-    assert "PIP_PREFIX" not in os.environ, "PIP_PREFIX should not be set!"
-
     if TMP_DIR.exists():
         print(f"-> Removing folder {TMP_DIR}")
         shutil.rmtree(TMP_DIR)
@@ -29,24 +31,31 @@ if __name__ == "__main__":
     print(f"-> Creating folder {TMP_DIR}")
     TMP_DIR.mkdir()
 
+    print(f"-> Copying project files to {TMP_DIR}")
+    shutil.copyfile(PROJECT_DIR / "pyproject.toml", TMP_DIR / "pyproject.toml")
+    shutil.copyfile(PROJECT_DIR / "uv.lock", TMP_DIR / "uv.lock")
+
     print(f"-> Installing packages into {TMP_DIR}")
     subprocess.run(
-        f"uv pip install -r reqs/requirements.txt --target {TMP_DIR}".split(),
+        f"uv sync --frozen --no-install-project --project {TMP_DIR}".split(),
         check=True,
     )
     subprocess.run(
-        f"uv pip install . --no-deps --target {TMP_DIR}".split(),
+        f"uv pip install . --no-deps --python {TMP_DIR}/.venv/".split(),
         check=True,
     )
 
-    print(f"-> Copying lambda handler to {TMP_DIR}")
-    shutil.copyfile(HANDLER, TMP_DIR / HANDLER.name)
+    print(f"-> Copying lambda handler to {DST_DIR}")
+    shutil.copyfile(HANDLER, DST_DIR / HANDLER.name)
 
     print(f"-> Creating zip archive {ARCHIVE}")
     subprocess.run(
-        ["bash", "-c", f"pushd {TMP_DIR} && zip -q -r {ARCHIVE} * && popd"],
+        ["bash", "-c", f"pushd {DST_DIR} && zip -q -r {ARCHIVE} * && popd"],
         check=True,
     )
+
+    print(f"-> Removing folder {TMP_DIR}")
+    shutil.rmtree(TMP_DIR)
 
     s3 = boto3.client("s3")
     buckets = s3.list_buckets()["Buckets"]
